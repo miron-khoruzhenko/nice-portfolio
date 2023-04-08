249c51c4abb755dfa40302c06a8311b4b5b37797
Safak <safakkel1903@gmail.com>
2023-04-08 21:22:43 +0300

    safak updated

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const navbar = document.querySelector('#navbar');
+const navbar1 = document.querySelector('#navbar1');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+    navbar.style.top = "0";
+    navbar1.style.top = "-100px";
+
+  } 
+  else {
+      navbar.style.top = "-100px";
+      navbar1.style.top = "0";
+}
+  console.log(`de:${document.documentElement.scrollTop}\nb: ${document.body.scrollTop}`)
+}
